c6aba8bca45afe35a3f8042405e319355ebebb99
CtheBoyd <99106917+CtheBoyd@users.noreply.github.com>
2022-07-28 16:03:57 -0700

    update

--- count_seq.py
+++ count_seq.py
@@ -1,1 +1,18 @@
-a
+def count_seq():
+    number = '2'
+    while True:
+        yield str(number)
+        next_value = ''
+        while len(number) > 0:
+            first = number[0]
+            count = 0
+            while len(number) > 0 and number[0] == first:
+                count += 1
+                number = number[1:]
+            next_value += '{}{}'.format(count, first)
+        number = next_value
+
+if __name__ =="__main__":
+    gen = count_seq()
+    for i in range(8):
+        print(next(gen))
