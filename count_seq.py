93534a3fe97ae5c63a43e7f8a62e2a865c49a4cb
CtheBoyd <99106917+CtheBoyd@users.noreply.github.com>
2022-07-28 20:40:37 -0700

    update

--- count_seq.py
+++ count_seq.py
@@ -1,16 +1,34 @@
+# Author: Christopher Boyd
+# GitHub username: CtheBoyd
+# Date:7/28/2022
+# Description: Generator function. that doesn't require any arguments and generates a sequence
+# that starts like this: 2, 12, 1112, 3112, 132112, 1113122112, 311311222112, 13211321322112, ...
+#
+
+
 def count_seq():
-    number = '2'
+    yield "2"
+    yield "12"
+    value = 3
     while True:
-        yield str(number)
-        next_value = ''
-        while len(number) > 0:
-            first = number[0]
-            count = 0
-            while len(number) > 0 and number[0] == first:
-                count += 1
-                number = number[1:]
-            next_value += '{}{}'.format(count, first)
-        number = next_value
+        nums = "12"
+        for i in range(3, value+1):
+            nums += "$"
+            length = len(nums)
+            count = 1
+            temp = ""
+            for y in range(1, length):
+                if nums[y] != nums[y-1]:
+                    temp += str(count)
+                    temp += nums[y-1]
+                    count = 1
+                else:
+                    count += 1
+            nums = temp
+        yield nums
+        value += 1
+
+
 
 if __name__ =="__main__":
     gen = count_seq()
